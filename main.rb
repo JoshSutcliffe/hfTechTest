179391a284dcaff406c94a4422ca98553c24e33b
JoshSutcliffe <joshclement12@gmail.com>
2016-04-25 15:51:10 +0100

    working on API

--- main.rb
+++ main.rb
@@ -4,5 +4,12 @@ require 'httparty'
 require 'pry'
 
 get '/' do
-  erb :index
+
+  url = "http://index1.homeflow.co.uk/properties/4631183?api_key=77467477edfd2689cd77796a2c4b019f"
+  @property_results = HTTParty.get(url) #this is capitalised
+  @result = @property_results['result']
+
+  erb(:about)
+
+  # erb :index
 end
